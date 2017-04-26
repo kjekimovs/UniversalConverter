107807266cfffd474613bcb70d8fac283970e863
kjekimovs@evolutiongaming.com <kjekimovs@evolutiongaming.com>
2017-04-26 20:45:18 +0300

    added parameterized

--- src/main/test/uconverter/CelsiusUnitConverterTest.java
+++ src/main/test/uconverter/CelsiusUnitConverterTest.java
@@ -6,7 +6,6 @@ import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 
 import static org.junit.Assert.*;
 
@@ -14,34 +13,33 @@ import static org.junit.Assert.*;
  * Created by JavaCourses on 26.04.2017..
  */
 @RunWith(Parameterized.class)
-public class KelvinUnitConverterTest {
-    KelvinUnitConverter converter = new KelvinUnitConverter();
+public class CelsiusUnitConverterTest {
+    CelsiusUnitConverter converter = new CelsiusUnitConverter();
     @Parameterized.Parameter(0)
-    public double srcKelvin;
+    public double celsius;
     @Parameterized.Parameter(1)
-    public double trgKelvin;
+    public double kelvin;
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {10.0, 10.0},
-                {100.0, 100.0},
-                {150.0, 150.0}
+                {0.0, 273.15},
+                {-100.0, 173.15},
+                {150.0, 423.15}
         });
     }
-
     public void toSI() throws Exception {
-        assertEquals(trgKelvin, converter.toSI(srcKelvin), 0.00001);
+        assertEquals(kelvin, converter.toSI (celsius), 0.00001);
     }
 
     @Test
     public void fromSI() throws Exception {
-        assertEquals(srcKelvin, converter.fromSI(trgKelvin), 0.00001);
+        assertEquals(celsius, converter.fromSI (kelvin), 0.00001);
     }
 
     @Test
     public void getName() throws Exception {
-        assertEquals("Kelvin", converter.getName());
+        assertEquals("Celsius", converter.getName());
     }
 
 }
